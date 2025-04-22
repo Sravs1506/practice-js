38f5f08745234da5d4cb5e226cddcd94db13b68e
Sravs1506 <sravyaullamgunta06@gmail.com>
2025-04-22 09:52:05 +0000

    conversion of datatypes

--- 01_basics/conv.js
+++ 01_basics/conv.js
@@ -1,3 +1,14 @@
-let isLoggedIn = 1
+let isLoggedIn = ""
 
 let booleanIsLoggedIn  = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "something" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
